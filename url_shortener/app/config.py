6481bedc24d999447d482a2f99d8afc3b63c45df
vladislav r <109311685+trashstarking@users.noreply.github.com>
2026-03-09 17:27:36 +0300

    Update config.py

--- url_shortener/app/config.py
+++ url_shortener/app/config.py
@@ -1,8 +1,9 @@
 from pydantic_settings import BaseSettings
+from typing import Optional
 
 class Settings(BaseSettings):
     DATABASE_URL: str
-    REDIS_URL: str
+    REDIS_URL: Optional[str] = None
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
